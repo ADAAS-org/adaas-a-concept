f43b34665e85acb00695697bbd1dd7e88c37c4dd
Andrew T <andrew.t@adaas.org>
2025-09-09 09:18:13 -0300

    new version created ::  0.0.46

--- dist/src/global/A-Entity/A-Entity.meta.js
+++ dist/src/global/A-Entity/A-Entity.meta.js
@@ -2,7 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.A_EntityMeta = void 0;
 const A_Meta_class_1 = require("../A-Meta/A-Meta.class");
+const A_Entity_types_1 = require("./A-Entity.types");
 class A_EntityMeta extends A_Meta_class_1.A_Meta {
+    /**
+     * Returns all features defined in the Container
+     *
+     * @returns
+     */
+    features() {
+        const features = this.get(A_Entity_types_1.A_TYPES__EntityMetaKey.FEATURES);
+        return (features === null || features === void 0 ? void 0 : features.toArray().map(([, feature]) => feature)) || [];
+    }
 }
 exports.A_EntityMeta = A_EntityMeta;
 //# sourceMappingURL=A-Entity.meta.js.map
--- src/global/A-Entity/A-Entity.meta.ts
+++ src/global/A-Entity/A-Entity.meta.ts
@@ -1,10 +1,23 @@
 import { A_TYPES__A_InjectDecorator_Meta } from "@adaas/a-concept/decorators/A-Inject/A-Inject.decorator.types";
 import { A_Meta } from "../A-Meta/A-Meta.class";
 import { A_TYPES__EntityMeta, A_TYPES__EntityMetaKey } from "./A-Entity.types";
+import { A_TYPES__A_DefineDecorator_Meta } from "@adaas/a-concept/decorators/A-Feature/A-Feature.decorator.types";
 
 
 export class A_EntityMeta extends A_Meta<A_TYPES__EntityMeta> {
 
-  
+    /**
+     * Returns all features defined in the Container
+     * 
+     * @returns 
+     */
+    features(): Array<A_TYPES__A_DefineDecorator_Meta> {
+
+        const features = this.get(A_TYPES__EntityMetaKey.FEATURES);
+
+        return features?.toArray()
+            // returns all extensions that match the feature
+            .map(([, feature]) => feature) || [];
+    }
 
 }
